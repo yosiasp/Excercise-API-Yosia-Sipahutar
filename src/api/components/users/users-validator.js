8fce903d01b007c9961ba0b56c8fbd51269f2eac
yosiasp <yosia.535230117@stu.untar.ac.id>
2024-04-05 21:56:15 +0700

    Mengubah validator untuk nomor 2

--- src/api/components/users/users-validator.js
+++ src/api/components/users/users-validator.js
@@ -6,6 +6,12 @@ module.exports = {
       name: joi.string().min(1).max(100).required().label('Name'),
       email: joi.string().email().required().label('Email'),
       password: joi.string().min(6).max(32).required().label('Password'),
+      password_confirm: joi
+        .string()
+        .min(6)
+        .max(32)
+        .required()
+        .label('Password confirm'),
     },
   },
 
